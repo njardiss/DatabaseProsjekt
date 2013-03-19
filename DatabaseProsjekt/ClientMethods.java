066ef4c27814beb3dfe9e4401b6c3f06b24fc57e
RuneMac <kjelaasr@gmail.com>
2013-03-19 16:08:12 +0100

    fikset på setNewDish

--- DatabaseProsjekt/ClientMethods.java
+++ DatabaseProsjekt/ClientMethods.java
@@ -1,24 +1,19 @@
 class clientMethods {
 	public String regNewCustomer(String name, int phone, String adress, String deliveryAdress, int type) {
-		String sql = "INSERT INTO customer(name, phone, adress, deliveryadress, type) values(('" + name + "'),
-		('" + phone + "'), ('" + adress + "'), ('" + deliveryAdress + "'),('" + type + "'))";
+		String sql = "INSERT INTO customer(name, phone, adress, deliveryadress, type) values('" + name + "',
+		" + phone + ", '" + adress + "', '" + deliveryAdress + "'," + type + ")";
 		return sql;
 	}
-	
-	// njardiss sicke ducks
 	public String getCustomer(int kid) {
-		String sql = "SELECT * from customer where kid = ('" + kid + "')";
+		String sql = "SELECT * from customer where kid = " + kid + "";
 		return sql;
 	}
-	
 	public String findOrders(int kid) {
-		String sql = "SELECT * from orders where kid = ('" + kid + "')";
+		String sql = "SELECT * from orders where kid = " + kid + "";
 		return sql;
 	}
-	
-	public String setNewDish(Dishes newDish) {
-	 	String sql = "INSERT INTO Dish(dishID, name, ingredients, price)values('" + newDish.getDishID() + "',
-		'" + newDish.getName() + "', '" + newDish.getIngredients() + "', '" + newDish.getPrice();
+	public String setNewDish(String name, String ingredients, double price){
+	 	String sql = "INSERT INTO Dish(name, ingredients, price)values('" + name + "',
+		'" + ingredients + "', '" + price + "');
     return sql;
- }
 }
