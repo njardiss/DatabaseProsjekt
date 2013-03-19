24808e41bb2de7dd82bd0f4015a62044d9858c3d
Jan Erik <janerik_3@msn.com>
2013-03-19 15:23:11 +0100

    dunno om nåkka funke at all

--- DatabaseProsjekt/ClientMethods.java
+++ DatabaseProsjekt/ClientMethods.java
@@ -1,19 +1,23 @@
 class clientMethods {
 	public String regNewCustomer(String name, int phone, String adress, String deliveryAdress, int type) {
-		String sql = "INSERT INTO customer(name, phone, adress, deliveryadress, type) values('" + name + "',
-		" + phone + ", '" + adress + "', '" + deliveryAdress + "'," + type + ")";
+		String sql = "INSERT INTO customer(name, phone, adress, deliveryadress, type) values(('" + name + "'),
+		('" + phone + "'), ('" + adress + "'), ('" + deliveryAdress + "'),('" + type + "'))";
 		return sql;
 	}
+	
 	public String getCustomer(int kid) {
-		String sql = "SELECT * from customer where kid = " + kid + "";
+		String sql = "SELECT * from customer where kid = ('" + kid + "')";
 		return sql;
 	}
+	
 	public String findOrders(int kid) {
-		String sql = "SELECT * from orders where kid = " + kid + "";
+		String sql = "SELECT * from orders where kid = ('" + kid + "')";
 		return sql;
 	}
-	public String setNewDish(Dish newDish)
+	
+	public String setNewDish(Dishes newDish) {
 	 	String sql = "INSERT INTO Dish(dishID, name, ingredients, price)values('" + newDish.getDishID() + "',
 		'" + newDish.getName() + "', '" + newDish.getIngredients() + "', '" + newDish.getPrice();
     return sql;
+ }
 }
