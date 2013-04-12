86ce7502dd9ad470e714d3b04b803ff12b32494a
njardiss <martinkastnes@msn.com>
2013-04-12 14:41:59 +0200

    Sletter classes

---
+++ DatabaseProsjekt/Customer.java
@@ -0,0 +1,49 @@
+class Customer {
+	private int kid;
+	private String name;
+	private int phone;
+	private String adress;
+	private final int type;
+
+	public Customer(int kid, String name, int phone, String adress, int type) {
+		this.kid = kid;
+		this.name = name;
+		this.phone = phone;
+		this.adress =  adress;
+		this.type = type;
+	}
+	public int getKid() {
+		return kid;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public int getType() {
+		return type;
+	}
+	public String setName(String newName) {
+		name = newName;
+		String sql = "update customer set name = '" + name + "' where kid = " + kid + "";
+		return sql;
+	}
+	public String setPhone(int newPhone) {
+		phone = newPhone;
+		String sql = "update customer set phone = '" + phone + "' where kid = " + kid + "";
+		return sql;
+	}
+	public String setAdress(String newAdress) {
+		adress = newAdress;
+		String sql = "update customer set adress = '" + adress + "' where kid = " + kid + "";
+		return sql;
+	}
+	public String toString() {
+		String x = "Kid: " + kid + ". Name: " + name + ". Phone: " + phone + ". Adress: " + adress + ". Type: " + type;
+		return x;
+	}
+}
