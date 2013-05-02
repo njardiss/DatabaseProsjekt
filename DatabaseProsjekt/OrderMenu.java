30bffff9aaaba91805f249f4db0e43533912dfb6
RuneMac <kjelaasr@gmail.com>
2013-05-02 15:29:56 +0200

    dg

--- DatabaseProsjekt/OrderMenu.java
+++ DatabaseProsjekt/OrderMenu.java
@@ -187,7 +187,7 @@ public class OrderMenu extends BasicDialog {
 	    		minutes.requestFocusInWindow();
 	    	} 
 	    	return false;
-	    }else if(deliveryAdress2.matches("[a-z1-9]")== false){
+	    }else if(deliveryAdress2.matches("[a-zA-Z]")== false){
 	    	showMessageDialog(OrderMenu.this, "Illegal input!");
 	    	deliveryAdress.requestFocusInWindow();
 	    	return false;
