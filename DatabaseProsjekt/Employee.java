623a8468e7bbd62b49aa89452040962e300783ea
njardiss <martinkastnes@msn.com>
2013-05-02 15:50:52 +0200

    Endringer parametre

--- DatabaseProsjekt/Employee.java
+++ DatabaseProsjekt/Employee.java
@@ -2,16 +2,21 @@ import java.util.ArrayList;
 import java.sql.*;
 
 public class Employee {
-	private final String birthdate;
+	private int phone;
+	private String name;
 	private double monthlySalary;
 	private final String dateHired;
-	public Employee(String birthdate, double monthlySalary, String dateHired) {
-		this.birthdate = birthdate;
+	public Employee(int phone, String name, double monthlySalary, String dateHired) {
+		this.phone = phone;
+		this.name = name;
 		this.monthlySalary = monthlySalary;
 		this.dateHired = dateHired;
 	}
-	public String getBirthdate() {
-		return birthdate;
+	public int getPhone() {
+		return phone;
+	}
+	public String getName() {
+		return name;
 	}
 	public double getMonthlySalary() {
 		return monthlySalary;
@@ -19,20 +24,28 @@ public class Employee {
 	public String getDateHired() {
 		return dateHired;
 	}
+	public boolean setPhone(int newPhone) {
+		phone = newPhone;
+		return true;
+	}
+	public boolean setName(String newName){
+		name = newName;
+		return true;
+	}
 	public boolean setSalary(double newSalary) {
 		monthlySalary = newSalary;
 		return true;
 	}
 }
 class Chef extends Employee {
-	public Chef(String birthdate, double monthlySalary, String dateHired) {
-		super(birthdate, monthlySalary, dateHired);
+	public Chef(int phone, String name, double monthlySalary, String dateHired) {
+		super(phone, name, monthlySalary, dateHired);
 	}
 }
 class Salesman extends Employee {
 	private double commission;
-	public Salesman(String birthdate, double monthlySalary, String dateHired) {
-		super(birthdate, monthlySalary, dateHired);
+	public Salesman(int phone, String name, double monthlySalary, String dateHired) {
+		super(phone, name, monthlySalary, dateHired);
 	}
 	public double getCommission() {
 		return commission;
@@ -43,17 +56,17 @@ class Salesman extends Employee {
 	}
 }
 class Driver extends Employee {
-	public Driver(String birthdate, double monthlySalary, String dateHired) {
-		super(birthdate, monthlySalary, dateHired);
+	public Driver(int phone, String name, double monthlySalary, String dateHired) {
+		super(phone, name, monthlySalary, dateHired);
 	}
 }
 class CEO extends Employee {
-	public CEO(String birthdate, double monthlySalary, String dateHired) {
-		super(birthdate, monthlySalary, dateHired);
+	public CEO(String birthdate, int phone, String name, double monthlySalary, String dateHired) {
+		super(phone, name, monthlySalary, dateHired);
 	}
 }
 class Secretary extends Employee {
-	public Secretary(String birthdate, double monthlySalary, String dateHired) {
-		super(birthdate, monthlySalary, dateHired);
+	public Secretary(String birthdate, int phone, String name, double monthlySalary, String dateHired) {
+		super(phone, name, monthlySalary, dateHired);
 	}
 }
